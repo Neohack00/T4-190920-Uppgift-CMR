12982b60329200056b6e3bd68f91ae285bfcfa14
Neohack00 <neo@bylock.se>
2019-09-20 11:43:31 +0200

    Created class Contact and initial Class properties

---
+++ Customer Relation Mangerment/Customer Relation Mangerment/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_Relation_Mangerment
+{
+    class Contact
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool Favorite { get; set; }
+    }
+}
